1e4f3ad0de68194effe7be908dab0934555875d4
Nkateko Maluleke <nkateko.maluleke03@gmail.com>
2024-03-02 11:34:22 +0200

    AppUser Identity declared

---
+++ Data Spider API/DbContext/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Data_Spider_API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data_Spider_API.DBContext
+{
+    public class DataContext : IdentityDbContext<AppUser>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.LazyLoadingEnabled = false;
+        }
+    }
+}
---
+++ Data Spider API/Models/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data_Spider_API.Models
+{
+    public class AppUser : IdentityUser
+    {
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+
+    }
+}
