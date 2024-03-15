8aaf7fb91d180b48a963a984871ba6e9c3612487
Nkateko Maluleke <nkateko.maluleke03@gmail.com>
2024-03-15 18:36:56 +0200

    Authentication controller implemented

---
+++ Data Spider API/Controllers/AuthenticationController.cs
@@ -0,0 +1,19 @@
+﻿using Data_Spider_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Data_Spider_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AuthenticationController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+    }
+}
